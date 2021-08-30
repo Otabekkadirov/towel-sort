1613da7330d6c68645e654802e189ef512b4b0c7
Otabek <74847963+Otabekkadirov@users.noreply.github.com>
2021-08-30 23:29:52 +0500

    fix: change function returns empty array if param is undefined

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
+    if (matrix == undefined) {
+        return [];
+    }
     let result = [];
 
     for (let row = 0; row < matrix.length; row++) {
         for (let column = 0; column < matrix[row].length; column++) {
-            result.push(matrix[row][column]);
+            let currentIndex =
+                row % 2 === 0 ? column : matrix[row].length - 1 - column;
+            result.push(matrix[row][currentIndex]);
         }
     }
     return result;
